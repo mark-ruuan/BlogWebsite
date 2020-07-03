a8424d3c6c7f51638b6a9efcf7f453a2889422dc
mark-ruuan <arunkk3127@gmail.com>
2020-07-04 00:55:35 +0530

    minor changes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
+/* eslint-disable */
 import React from 'react';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
 import './App.css';
 import Home from './containers/Home';
 import Header from './components/Header';
 import Hero from './components/Hero';
+import ContactUs from './containers/ContactUs';
+import Post from './containers/Post';
+
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Hero />
-      <Home />
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <Hero />
+        <Route path="/" exact component={Home}/>
+        <Route path="/contact-us" exact component={ContactUs} />
+        <Route path="/post/:postId" exact component={Post} />
+      </div>
+    </Router>
+    
   );
 }
 
