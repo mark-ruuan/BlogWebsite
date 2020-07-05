558dddd3c4c28477809bc91a0f75d00c3d5e168d
mark-ruuan <arunkk3127@gmail.com>
2020-07-06 00:32:22 +0530

    updated assessts

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ import Header from './components/Header';
 import Hero from './components/Hero';
 import ContactUs from './containers/ContactUs';
 import Post from './containers/Post';
+import Posts from './containers/Posts';
+import AboutUs from './containers/AboutUs';
 
 
 function App() {
@@ -18,6 +20,8 @@ function App() {
         <Hero />
         <Route path="/" exact component={Home}/>
         <Route path="/contact-us" exact component={ContactUs} />
+        <Route path="/about-us" exact component={AboutUs} />
+        <Route path="/posts" exact component={Posts} />
         <Route path="/post/:postId" exact component={Post} />
       </div>
     </Router>
